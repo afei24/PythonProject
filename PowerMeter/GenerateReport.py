84bb2896a44874a3121bdef91ba8bb14b5fc94ad
workRuby <ruby.@qq.com>
2019-03-22 11:17:16 +0800

    修改中文乱码

--- PowerMeter/GenerateReport.py
+++ PowerMeter/GenerateReport.py
@@ -4,25 +4,31 @@ import config
 import os
 import Report
 from datetime import datetime, date, timedelta
+import codecs
 
 MeterAddress = []
+MeterName = []
 reportPath = config.savePath +'\\'+ config.dtCurrentMonth+'.csv'
 meterDatas=[]
 
-with pymssql.connect("DESKTOP-QUO241A\SQLEXPRESS", "sa", "admin123", "Decathlon_Blueeye") as conn:
+with pymssql.connect("DESKTOP-QUO241A\SQLEXPRESS", "sa", "admin123", "Decathlon_Blueeye",charset='utf8') as conn:
     with conn.cursor() as cursor:
-        cursor.execute('SELECT [Address] FROM MeterAddress ')
+        cursor.execute('SELECT [Address],[Name] FROM MeterAddress ')
         row = cursor.fetchone()
         while row:
+            MeterName.append(row[1])
             MeterAddress.append(row[0])
             row = cursor.fetchone()
 
         createReport = Report.CreateReport(reportPath,MeterAddress)
+        print(MeterName)
         if not os.path.exists(reportPath):
             createReport.Create()
         else:
-            for address in MeterAddress:
-                meterData = [config.dtCurrentDay,int(address)]
+            for i in range(0,len(MeterAddress)):
+                name = MeterName[i]
+                address = MeterAddress[i]
+                meterData = [config.dtCurrentDay,name]
                 for timeRange in config.timeRanges:
                     sql = "SELECT TOP 1  PwTotal  FROM DLT_10H_F1_0001FF00 WHERE [Address]='%s' and ([CreateTime]<'%s' or [CreateTime]='%s') order by CreateTime desc" % \
                           (address, timeRange[0], timeRange[0])
